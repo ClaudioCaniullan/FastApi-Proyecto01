1ced9d6a2f3779f6a33358b625fef10f11c96cd8
ClaudioCaniullan <claudio.caniullan@usach.cl>
2023-07-06 19:53:25 -0400

    validations_query

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from pydantic import BaseModel
 
 #Fastapi
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
+
 
 # crear una instacia Fastapi
 app = FastAPI()
@@ -34,3 +35,12 @@ def Home():
 def create_person(person: Person = Body()):
     return person 
 
+
+
+# Validaciones: Query Parameters
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age : str = Query(...) # parametro obligatorio (aunque en todo query parameter debe ser opcional)
+):
+    return {name: age}
