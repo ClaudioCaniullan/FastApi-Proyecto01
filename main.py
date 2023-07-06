3f75548a74686186285e25f6803a4e06073b7a08
ClaudioCaniullan <claudio.caniullan@usach.cl>
2023-07-05 23:57:00 -0400

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 # importar librerias
+
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#Fastapi
 from fastapi import FastAPI
+from fastapi import Body
 
 # crear una instacia Fastapi
 app = FastAPI()
+
+#Models
+class Person(BaseModel):
+
+    first_name = str
+    last_name = str
+    age : int
+    hair_color : Optional[str] = None
+    is_married : Optional[bool] = None
 
 # crear path operations
 @app.get("/")
@@ -10,3 +28,9 @@ def Home():
     return {"Hello": "world"}
 
 # correr proyecto en consola: uvicorn main:app --reload
+
+# Request and Response Body
+@app.post("/person/new")
+def create_person(person: Person = Body()):
+    return person 
+
