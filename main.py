80f000906253c67806c605a5d80d7168cd4087cb
ClaudioCaniullan <claudio.caniullan@usach.cl>
2023-07-08 19:52:45 -0400

    validations_body

--- main.py
+++ main.py
@@ -15,10 +15,16 @@ from fastapi import Body, Query, Path
 app = FastAPI()
 
 #Models
+
+class Location(BaseModel): 
+    city : str
+    state: str
+    country: str
+
 class Person(BaseModel):
 
-    first_name = str
-    last_name = str
+    first_name : str 
+    last_name : str
     age : int
     hair_color : Optional[str] = None
     is_married : Optional[bool] = None
@@ -60,3 +66,20 @@ def show_person(
     person_id : int = Path(...,gt= 0) #path parameter obligatorio
 ):
     return {person_id: "It exists"}
+
+
+# Validaciones: Request Body
+@app.put("/person/{person_id}")
+def update_person(
+    person_id : int = Path(
+    ..., 
+    title = "Person ID", 
+    description = "This is de person ID", 
+    gt = 0
+    ),
+    person: Person = Body(...),
+    location : Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
